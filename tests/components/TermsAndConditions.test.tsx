47f06130cbd9d908e034ae7e52f9920bf7d84ac3
gasparmi <gaspar.mi@outlook.com>
2025-06-12 17:57:38 -0700

    Simplified tests for TermsAndConditions component

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,22 +3,36 @@ import TermsAndConditions from "../../src/components/TermsAndConditions";
 import userEvent from "@testing-library/user-event";
 
 describe("TermsAndConditions", () => {
-  it("should render with correct text and initial state", () => {
+  // Simplifying Tests
+  const renderComponent = () => {
     render(<TermsAndConditions />);
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+    };
+  };
+
+  it("should render with correct text and initial state", () => {
+    const { heading, checkbox, button } = renderComponent();
+    // render(<TermsAndConditions />);
+
+    // const heading = screen.getByRole("heading");
+
+    // NO LONGER NEEDED BECAUSE ABOVE RETURN WOULD FAIL (first), if it's not in the DOM
+    // expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent("Terms & Conditions");
 
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).toBeInTheDocument();
+    // const checkbox = screen.getByRole("checkbox");
+    // expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
 
     // If we had more than one button, we can specify its name
     // const button = screen.getByRole("button", {name: /submit/i});
 
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
+    // const button = screen.getByRole("button");
+    // expect(button).toBeInTheDocument();
     // expect(button).toHaveTextContent(/submit/i);
     expect(button).toBeDisabled();
   });
@@ -26,14 +40,15 @@ describe("TermsAndConditions", () => {
   // USER INTERACTIONS
   it("should enable the button when the checkbox is checked", async () => {
     // Arrange part
-    render(<TermsAndConditions />);
+    // render(<TermsAndConditions />);
+    const { checkbox, button } = renderComponent();
 
     // Action
-    const checkbox = screen.getByRole("checkbox");
+    // const checkbox = screen.getByRole("checkbox");
     const user = userEvent.setup();
     await user.click(checkbox);
 
     // Assert
-    expect(screen.getByRole("button")).toBeEnabled();
+    expect(button).toBeEnabled();
   });
 });
